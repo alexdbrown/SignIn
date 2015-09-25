78dbcf3b652620cd3320114399f68bf77f7f0ece
alexandra <alexdbrown@gmail.com>
2015-09-25 10:15:32 -0700

    add signIn function in factory

--- services/CourseFactory.js
+++ services/CourseFactory.js
@@ -1,13 +1,20 @@
 SignIn.factory('CourseFactory', function CourseFactory(){
   var factory = {};
-  factory.students = [{ name: Jerry Seinfeld, signedIn: False }];
-  factory.students = [{ name: Elaine Benes, signedIn: False }];
-  factory.students = [{ name: Kramer, signedIn: False }];
-  factory.students = [{ name: George Costanza, signedIn: False }];
-  factory.students = [{ name: Newman, signedIn: False }];
-  factory.students = [{ name: Uncle Leo, signedIn: False }];
-  factory.students = [{ name: Larry David, signedIn: False }];
+  factory.students = [{ name: 'Jerry Seinfeld', signedIn: False },
+                      { name: 'Elaine Benes', signedIn: False },
+                      { name: 'Kramer', signedIn: False },
+                      { name: 'George Costanza', signedIn: False },
+                      { name: 'Newman', signedIn: False },
+                      { name: 'Uncle Leo', signedIn: False },
+                      { name: 'Larry David', signedIn: False }];
 
+  $scope.signIn = function(student {
+    if (student.signedIn == false) {
+      student.signedIn = true;
+    } else {
+      student.signedIn = false;
+    }
+  });
 
   return factory;
 });
